7a2ec8a9d4a5a052a8c9df11169d2da978bd2f83
Daniel McPherson <dam344@txstate.edu>
2021-04-27 10:42:03 -0500

    deleted unused file

--- src/Connect4.java
+++
@@ -1,8 +0,0 @@
-public class Connect4 {
-    //GameGUI gui;
-    //gameMGR mgr;
-    //Board board;
-
-    //public Connect4(){ gui = new GameGUI(); }
-    //public Connect4(){mgr = new gameMGR(GameGui gui, Board board);}
-}
